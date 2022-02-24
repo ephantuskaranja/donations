4652f18898369f576b610c6e01add65a75534ebc
Ephantus karanja <ephantusslogan@gmail.com>
2022-02-24 08:19:27 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Donations</title>
+</head>
+<body>
+    <div class="container">
+        <h3>PesaPal Donate</h3>
+        <div class="row mt-5">
+            <div class="col-md-8 card">
+                <div class="card-header">
+                    Personal Details
+                </div>
+                <div class="card-body">
+                    <button type="button" class="btn btn-primary">Primary</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+    
+</body>
+</html>
